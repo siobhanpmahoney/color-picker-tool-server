269d34d42dfc2628536242a18f4ce158ab706ebc
siobhanpmahoney <siobhan.p.mahoney@gmail.com>
2019-01-21 17:58:36 -0500

    added default val for as_gradient

--- db/schema.rb
+++ db/schema.rb
@@ -22,7 +22,7 @@ ActiveRecord::Schema.define(version: 2019_01_20_193438) do
     t.string "website_placement"
     t.string "status"
     t.text "notes"
-    t.boolean "as_gradient"
+    t.boolean "as_gradient", default: false
     t.string "gradient_css"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
